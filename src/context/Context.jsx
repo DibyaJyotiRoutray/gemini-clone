dce63de7020f3b225a296f820716049899025d71
Dibya Jyoti Routray <dibyajyotiroutray68@gmail.com>
2024-08-17 14:35:22 +0530

    added the google api key

---
+++ src/context/Context.jsx
@@ -0,0 +1,23 @@
+import { createContext } from "react";
+import run from "../config/gemini";
+
+const Context = createContext();
+
+const ContextProvider = (props) => {
+    const onSent = async (prompt) => {
+        await run(prompt)
+    } 
+    
+    onSent("what is React.js")
+    const contextValue = {
+
+    }
+
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+}
+
+export default ContextProvider
